907eff84db2cdd9595e188577b211bf688afeadd
abubakar <abubakarfarah011@gmail.com>
2025-12-26 21:31:45 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ const form = document.getElementById("create-task-form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const taskDescription = event.target["new-task-description"].value;
+
+  const li = document.createElement("li");
+  li.textContent = taskDescription;
+
+  const tasks = document.getElementById("tasks");
+  tasks.appendChild(li);
 });
+});
+
+  
+  
+  
